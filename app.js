13875cbe33d12807231cece0a7774c27a137c2c1
VinzRaj <vinny44@gmail.com>
2022-03-14 11:48:35 -0500

    basic express set up is done

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const notFound = require('./middlewares/not_found');
+const errorHandle = require('./middlewares/error_handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Helloo</h1><a>Store Api</a>');
+});
+app.use(notFound);
+app.use(errorHandle);
+
+const start = async () => {
+  try {
+    app.listen(3000, () => {
+      console.log('server is listening');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
